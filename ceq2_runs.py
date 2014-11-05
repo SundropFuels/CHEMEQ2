ce6954edfd9a38c828424684b3041a0c43ca9e83
Chris <chris.perkins@sundropfuels.com>
2014-11-05 08:33:44 -0700

    Made runner file capable of looping through multiple conditions and writing results to a csv.

--- ceq2_runs.py
+++ ceq2_runs.py
@@ -8,13 +8,18 @@ from collections import OrderedDict
 if __name__ == "__main__":
     t = np.arange(0, 0.1, 0.001)
 
-    ph = ct.Solution('POLIMI_TOT_1407_reduced.cti')
-    ph.TPX = 1200, 101325, 'CH4:1.0'
+    ph = ct.Solution('POLIMI_TOT_1407.cti')
+    temps = range(1000,1450,50)
+    pressures = [101325, 101325*2, 101325*3]
+    for T in temps:
+        for P in pressures:
+            ph.TPX = T+273.15, P, 'CH4:1.0'
 
-    solver = kin.ChemEQ2Solver(ct_phase = ph)
-    solver.initialize(t)
-    solver.solve(Nc=4)
-        
-    t_p = np.arange(0,0.1,0.01)
-    print solver.y.loc[t_p, ['CH4','H2','C2H2','C2H4']]
+            solver = kin.ChemEQ2Solver(ct_phase = ph)
+            solver.initialize(t)
+            solver.solve(Nc=4)
+            filename = 'CH4_crack_no_oxidant_%s_%s' % (T, P/101325.0)
+            solver.y.to_csv(filename)
+    
+
     
